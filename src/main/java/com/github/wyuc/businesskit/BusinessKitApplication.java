9afc3bfabe057b64597dad90bde9d33618a28e66
wyuc <wyuc_ovo@qq.com>
2022-04-30 00:04:51 +0800

    feat(all): init project
    (cherry picked from commit 5d34d55cbe8c71a8004aa2a2a93c0c1a23a1a628)

---
+++ src/main/java/com/github/wyuc/businesskit/BusinessKitApplication.java
@@ -0,0 +1,13 @@
+package com.github.wyuc.businesskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessKitApplication.class, args);
+    }
+
+}
